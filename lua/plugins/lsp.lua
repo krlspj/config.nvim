62e0b5fcfcaf58ceedbe076348696adc15e42bce
carles.pujades <carles.pujades@alisys.net>
2026-05-07 00:36:01 +0200

    lsp rename

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,15 +26,15 @@ return {
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
           end
 
-          map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
-          map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
-          map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-          map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-          map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-          map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          map("gn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("ga", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
           map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
           map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
-          map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+          map("gt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
 
           local function client_supports_method(client, method, bufnr)
             if vim.fn.has("nvim-0.11") == 1 then
